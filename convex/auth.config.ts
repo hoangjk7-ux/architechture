b61e5a319731fe860ce4200bf67a47979d1c9c5a
hoangjk7-ux <hoangjk7@gmail.com>
2026-07-29 06:35:22 +0000

    Update Convex auth provider domain

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: process.env.CONVEX_SITE_URL,
+      domain: "https://moonlit-warthog-945.convex.site",
       applicationID: "convex",
     },
   ],
