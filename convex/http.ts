c2135048301ffaf8df91966184d8c4bcdf964e7b
hoangjk7-ux <hoangjk7@gmail.com>
2026-08-03 07:57:11 +0000

    Fix OPTIONS handler signature and use request Origin for CORS

--- convex/http.ts
+++ convex/http.ts
@@ -9,11 +9,15 @@ auth.addHttpRoutes(http);
 http.route({
 	method: "OPTIONS",
 	path: "/api/auth/google",
-	handler: async () => {
+	handler: async (_ctx: any, request: Request) => {
+		const allowedOrigin = process.env.CONVEX_ALLOWED_ORIGIN || process.env.VITE_CONVEX_URL || process.env.CONVEX_SITE_URL || "*";
+		const originHeader = request.headers.get("Origin") ?? undefined;
+		const accessOrigin = originHeader ?? allowedOrigin;
+
 		return new Response(null, {
 			status: 204,
 			headers: {
-				"Access-Control-Allow-Origin": process.env.CONVEX_ALLOWED_ORIGIN || process.env.VITE_CONVEX_URL || process.env.CONVEX_SITE_URL || "*",
+				"Access-Control-Allow-Origin": accessOrigin,
 				"Access-Control-Allow-Methods": "POST, OPTIONS",
 				"Access-Control-Allow-Headers": "Content-Type",
 				"Access-Control-Allow-Credentials": "true",
